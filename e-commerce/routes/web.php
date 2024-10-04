e75d0e8ed1c1bacfec193ebed9437b9c397204f4
ZetsubouCode <kignatius80@gmail.com>
2024-10-04 13:30:35 +0700

    Add base routes

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -1,7 +1,43 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+})->name('home');
+
+// Cart
+Route::get('/cart', function () {
+    return view('cart');
+})->name('cart');
+
+// Checkout
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+// Checkout
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+// Shop
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
+Route::get('/shop-detail', function () {
+    return view('shop-detail');
+})->name('shop-detail');
+
+// Generate PDF
+Route::get('/generate-pdf', function () {
+    // Data to pass to the view
+    $data = ['title' => 'Welcome to PDF Generation in Laravel'];
+
+    // Load view file and pass data to it
+    $pdf = Pdf::loadView('pdf.myPDF', $data);
+
+    // Stream the PDF in the browser (can use download() for downloading)
+    return $pdf->stream('document.pdf');
 });
